42d074e92740087de60923113abee10678035803
xSamuel31x <61188858+xSamuel31x@users.noreply.github.com>
2024-02-23 11:42:42 +0100

    Version ES Actualizada

--- locales/es.lua
+++ locales/es.lua
@@ -1,31 +1,31 @@
 local Translations = {
     error = {
         canceled = 'Cancelado',
-        max_ammo = 'Munición al máximo',
-        no_weapon = 'No tienes ningún arma.',
-        no_support_attachment = 'Este arma no puede llevar este accesorio.',
-        no_weapon_in_hand = 'No tienes ningún arma en la mano.',
-        weapon_broken = 'Este arma esta estropeada y no puedes usarla.',
-        no_damage_on_weapon = 'Este arma no esta dañada..',
-        weapon_broken_need_repair = 'Tu arma esta estropeada, necesitas repararla antes de poder usarla.',
-        attachment_already_on_weapon = 'Ya tienes un %{value} en tu arma.'
+        max_ammo = 'Municion Maxima',
+        no_weapon = 'No tienes un arma.',
+        no_support_attachment = 'Esta arma no soporta este accesorio.',
+        no_weapon_in_hand = 'No tienes un arma en la mano.',
+        weapon_broken = 'Esta arma está rota y no se puede usar.',
+        no_damage_on_weapon = 'Esta arma no está dañada.',
+        weapon_broken_need_repair = 'Tu arma está rota, necesitas repararla antes de poder usarla de nuevo.',
+        attachment_already_on_weapon = 'Ya tienes un/a %{value} en tu arma.'
     },
     success = {
-        reloaded = 'Recargada'
+        reloaded = 'Recargado'
     },
     info = {
-        loading_bullets = 'Cargando Munición',
-        repairshop_not_usable = 'La tienda de reparación en este momento ~r~NO~w~ puede ser usada.',
-        weapon_will_repair = 'Tu arma sera reparada.',
-        take_weapon_back = '[E] - Coger Tu Arma',
+        loading_bullets = 'Cargando Balas',
+        repairshop_not_usable = 'El taller de reparaciones en este momento no está ~r~DISPONIBLE~w~.',
+        weapon_will_repair = 'Tu arma será reparada.',
+        take_weapon_back = '[E] - Recoger el Arma',
         repair_weapon_price = '[E] Reparar Arma, ~g~$%{value}~w~',
-        removed_attachment = 'Has quitado el %{value} de tu arma!',
+        removed_attachment = '¡Has quitado %{value} de tu arma!',
         hp_of_weapon = 'Durabilidad de tu arma'
     },
     mail = {
         sender = 'Tyrone',
         subject = 'Reparación',
-        message = 'Tu %{value} ha sido reparada puedes recogarla en esta ubicación. <br><br> Espavila matao'
+        message = 'Tu %{value} ha sido reparado, puedes recogerlo en la ubicación. <br><br> Hasta la próxima, amigo.'
     },
 }
 
